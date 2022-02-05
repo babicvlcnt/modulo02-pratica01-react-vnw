01b52b9662d32ecb44202945c4ef480c8e4f1bd0
Bárbara Cavalcante <93950101+babicvlcnt@users.noreply.github.com>
2022-02-05 12:34:54 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Oi extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Olá, mundo!</h1>
+      </div>
+    );
+  }
+}
+export default Oi;
